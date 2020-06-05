7b37d83bc3430e472fb6191ee2c0c1f980411302
IEIrodov <nikhilindia30111999@gmail.com>
2020-06-06 04:04:24 +0530

    Completing the Project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,29 +2,23 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
+function createEntry(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      content={emojiTerm.meaning}
+    />
+  );
+}
 function App() {
   return (
     <div>
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">
-        <Entry
-          emoji={emojipedia[0].emoji}
-          name={emojipedia[0].name}
-          content={emojipedia[0].meaning}
-        />
-        <Entry
-          emoji={emojipedia[1].emoji}
-          name={emojipedia[1].name}
-          content={emojipedia[1].meaning}
-        />
-        <Entry
-          emoji={emojipedia[1].emoji}
-          name={emojipedia[1].name}
-          content={emojipedia[1].meaning}
-        />
-      </dl>
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
     </div>
   );
 }
